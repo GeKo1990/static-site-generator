d2a3b69ec77a1c91fa5f46bcf0a3fd0bd648fbd5
GeKo1990 <geko1990@gmx.at>
2024-05-24 14:02:31 +0200

    added first unittest

---
+++ src/test_textnode.py
@@ -0,0 +1,23 @@
+import unittest
+
+from textnode import TextNode
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "bold")
+        self.assertEqual(node, node2)
+    
+    def test_eq1(self):
+        node = TextNode("This is a text node", "bold", "My Url")
+        node2 = TextNode("This is a text node", "bold")
+        self.assertNotEqual(node, node2)
+    
+    def test_eq2(self):
+        node = TextNode("This is a text node", "cursive", "https://boot.dev")
+        node2 = TextNode("This is a text node", "bold", "https://boot.dev")
+
+
+if __name__ == "__main__":
+    unittest.main()
