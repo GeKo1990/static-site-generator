e29326738e31212c69fee8120d71c27edfffa7a8
GeKo1990 <geko1990@gmx.at>
2024-05-24 20:14:10 +0200

    textNodeToHTMLNode WIP

--- src/main.py
+++ src/main.py
@@ -1,7 +1,28 @@
 from textnode import TextNode
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 def main():
     print("hello world")
+
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+    if text_node == None:
+        raise ValueError("No TextNode provided")
+    if text_node.text_type not in supported_text_types():
+        raise ValueError(f'{text_node.text_type} is not supported')
+    
+    type = text_node.text_type
+    if type == "text_type_text":
+        return LeafNode(None, text_node.text)
+        
+    
+def supported_text_types() -> dict:
+    return {
+        "text_type_text" : "text",
+        "text_type_bold" : "bold",
+        "text_type_italic" : "italic",
+        "text_type_code" : "code",
+        "text_type_link" : "link",
+        "text_type_image" : "image"
+    }
 
 main()
--- src/textnode.py
+++ src/textnode.py
@@ -9,3 +9,13 @@ class TextNode:
     
     def __repr__(self) -> str:
         return f'TextNode({self.text}, {self.text_type}, {self.url})'
+    
+    def __supported_types(self) -> dict:
+        return {
+            "text_type_text" : "text",
+            "text_type_bold" : "bold",
+            "text_type_italic" : "italic",
+            "text_type_code" : "code",
+            "text_type_link" : "link",
+            "text_type_image" : "image"
+        }
