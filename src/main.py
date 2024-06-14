6b32bfeeddbf88edde6a8bf3a51aca1ad473779a
GeKo1990 <geko1990@gmx.at>
2024-06-14 14:35:41 +0200

    work in progress, still not working everything but progress will be made.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,22 @@
 import os
 import shutil
 
+from blocks import (
+    markdown_to_blocks,
+    block_to_block_type,
+    paragraph_to_html_node,
+    heading_to_html_node,
+    unordered_list_to_html_node,
+    ordered_list_to_html_node,
+    code_to_html_node,
+    quote_to_html_node,
+    block_to_html_node,
+    markdown_to_html_node
+)
+
 def main():
     copy_directory_contents("./static", "./public")
+    generate_page("./content/index.md", "./template.html", "./public/index.html")
 
 def clear_directory(directory):
     if os.path.exists(directory):
@@ -27,6 +41,33 @@ def copy_directory_contents(src, dst):
     
     recursive_copy(src, dst)
 
+def extract_title(markdown):
+    if (len(markdown) > 0 and markdown.startswith("# ")):
+        return markdown.splitlines()[0].lstrip("# ")
+    else:
+        raise Exception("No title found in markdown.")
+    
+def read_file(file_path):
+    with open(file_path, 'r') as file:
+        content = file.read()
+    return content
 
+def write_file(dest_path, content):
+    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+    
+    with open(dest_path, 'w') as file:
+        file.write(content)
+    
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}.")
+
+    markdown = read_file(from_path)
+    template = read_file(template_path)
+    html = markdown_to_html_node(markdown).to_html()
+    title = extract_title(markdown)
+
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html)
+    write_file(dest_path, template)
 
 main()
